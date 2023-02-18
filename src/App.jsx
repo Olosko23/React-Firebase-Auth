849134981f98b82d2ad11ace3412eb913b2fda43
Olosko23 <oloogeorge633@gmail.com>
2023-02-18 19:15:56 +0300

    'new'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import Header from './components/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-
   return (
-    <div className="">
-     Hello
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<Register />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
